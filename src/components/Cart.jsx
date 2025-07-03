83cb78d702b0265fba6ed794dfbceabbd9607507
Keshav Bagad <keshavbagad01@gmail.com>
2025-07-04 01:38:27 +0530

    Axios added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-
+ 
 const Cart = ({ cartItems, onRemoveFromCart }) => {
   const [localCart, setLocalCart] = useState(cartItems);
   useEffect(() => {
